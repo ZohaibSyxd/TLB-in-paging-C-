cad7679cf2c03e9073c99261787418b3d77688f9
zohaib <zohaib.syed@student.unimelb.edu.au>
2025-04-27 16:09:57 +1000

    Parsing Logical Address implementation

--- main.c
+++ main.c
@@ -1,3 +1,15 @@
-int main() {
+#include <stdio.h>
+#include <stdint.h>
+
+#define OFFSET_MASK 0xFFF
+#define PAGE_NUMBER_MASK 0x3FF
+
+void parse_logical_address(uint32_t logical_address);
+
+int main(){
+    uint32_t addr1 = 30023, addr2 = 10001, addr3 = 20007;
+    parse_logical_address(addr1);
+    parse_logical_address(addr2);
+    parse_logical_address(addr3);
     return 0;
 }
